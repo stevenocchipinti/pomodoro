74775925480c402e6b4ccca3e6997b95a8ea55c9
Steven Occhipinti <steve.occhipinti@rea-group.com>
2013-02-05 09:39:29 +1100

    Make the javascript use JQuery

--- javascript.js
+++ javascript.js
@@ -1,29 +1,36 @@
-var icon = 'icon.jpg';
-var snd = new Audio("file.wav");
-var countdown = parseInt(document.getElementById('countdown').innerHTML);
-var timer = 0;
+$(function($) {
 
-if (window.webkitNotifications.checkPermission() !== 0) {
-  document.getElementById("permission").innerHTML =
-    "Looks like you haven't enabled desktop notifications, <a href='#' onclick='window.webkitNotifications.requestPermission();'>click here to enable</a>";
-}
+  var icon = 'icon.jpg';
+  var snd = new Audio("file.wav");
+  var countdown = parseInt($('#countdown').html());
+  var timer = 0;
 
-function showPopup() {
-  if (window.webkitNotifications.checkPermission() == 0) {
-    var popup = window.webkitNotifications.createNotification(
-      icon, 'Pomodoro', 'Pomodoro complete - take a break!'
+  // Check for desktop notification permissions
+  if (window.webkitNotifications.checkPermission() !== 0)
+    $("#permission").html(
+      "Looks like you haven't enabled desktop notifications, " +
+      "<a href='#' onclick='window.webkitNotifications.requestPermission();'>" +
+      "click here to enable</a>"
     );
-    popup.show();
-  }
-  snd.play();
-}
 
-function tick() {
-  document.getElementById("countdown").innerHTML = --countdown;
-  if (countdown == 0) {
-    showPopup();
-    clearInterval(timer);
+  function showPopup() {
+    if (window.webkitNotifications.checkPermission() == 0) {
+      var popup = window.webkitNotifications.createNotification(
+        icon, 'Pomodoro', 'Pomodoro complete - take a break!'
+      );
+      popup.show();
+    }
+    snd.play();
   }
-}
 
-timer = setInterval(tick, 1000);
+  function tick() {
+    $("#countdown").html(--countdown);
+    if (countdown == 0) {
+      showPopup();
+      clearInterval(timer);
+    }
+  }
+
+  timer = setInterval(tick, 1000);
+
+});
