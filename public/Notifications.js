9b3c7e082984c1a872f609d02e3cff7df73965c9
Steven Occhipinti <dev@stevenocchipinti.com>
2013-02-10 21:17:38 +1100

    Fallback to alert() for non-webkit browsers

--- public/Notifications.js
+++ public/Notifications.js
@@ -1,22 +1,27 @@
 var Notifications = {
 
   setup: function() {
-    $("a#permission-request").click(function requestPermission() {
-      window.webkitNotifications.requestPermission();
-      $("#permission-request").hide();
-    });
-    if (window.webkitNotifications.checkPermission() !== 0) {
-      $("#permission-request").slideDown("slow");
+    if (window.webkitNotifications) {
+      $("a#permission-request").click(function requestPermission() {
+        window.webkitNotifications.requestPermission();
+        $("#permission-request").hide();
+      });
+      if (window.webkitNotifications.checkPermission() !== 0) {
+        $("#permission-request").slideDown("slow");
+      }
     }
   },
 
   show: function(title, text) {
     var icon = "icon.jpg";
-    var snd = new Audio("notification.wav");
-    if (window.webkitNotifications.checkPermission() == 0) {
-      window.webkitNotifications.createNotification(icon, title, text).show();
+    new Audio("notification.wav").play();
+    if (window.webkitNotifications) {
+      if (window.webkitNotifications.checkPermission() == 0) {
+        window.webkitNotifications.createNotification(icon, title, text).show();
+      }
+    } else {
+      alert(text);
     }
-    snd.play();
   }
 
 };
