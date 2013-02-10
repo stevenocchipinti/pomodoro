44838d1b25480a1ca4c19451efdd03b6c8ce70cb
Steven Occhipinti <dev@stevenocchipinti.com>
2013-02-10 23:42:36 +1100

    Proof of concept with WebSockets via Pusher

--- application.rb
+++ application.rb
@@ -9,3 +9,13 @@ end
 post '/' do
   notificationTime = params[:notificationTime]
 end
+
+get '/test' do
+  require 'pusher'
+
+  Pusher.app_id = 'APP_ID'
+  Pusher.key = 'API_KEY'
+  Pusher.secret = 'SECRET_KEY'
+
+  Pusher['my-channel'].trigger('my-event', {:message => 'hello world'})
+end
