d8480acaf5ac275733adecc90f61a46af91304ea
Steven Occhipinti <dev@stevenocchipinti.com>
2013-02-26 22:09:06 +1100

    Use a Session class on the server

--- application.rb
+++ application.rb
@@ -5,27 +5,54 @@ Pusher.app_id = ENV["PUSHER_APP_ID"]
 Pusher.key = ENV["PUSHER_KEY"]
 Pusher.secret = ENV["PUSHER_SECRET"]
 
-sessions = {
-  "default" => nil
-}
-
 
 get "/*" do |session_name|
-  session_name = "default" unless session_name && !session_name.empty?
+  session = Session.find(session_name)
+  # TODO: Use session.to_hash (beware of camel_case)
   erb :index, :locals => {
-    session_name: session_name,
-    notification_time: sessions[session_name]
+    session_name: session.name,
+    notification_time: session.notification_time,
+    duration: session.duration
   }
 end
 
-
 post "/" do
-  session_name = params[:sessionName]
-  session_name = "default" unless session_name && !session_name.empty?
-  sessions[session_name] = params[:notificationTime]
-  if sessions[session_name] && !sessions[session_name].empty?
-    Pusher[session_name].trigger("start", :notificationTime => sessions[session_name])
+  session = Session.find(params[:sessionName])
+  session.duration = params[:duration]
+  session.notification_time = params[:noticationTime]
+  if session.notification_time
+    Pusher[session.name].trigger("start", session.to_hash)
   else
-    Pusher[session_name].trigger("stop", {})
+    Pusher[session.name].trigger("stop", {})
   end
+end
+
+
+
+class Session
+  attr_accessor :name, :notification_time, :duration
+
+  @@sessions = {}
+
+  def initialize(name, notification_time=nil, duration=25)
+    @name = name
+    @notification_time = notification_time
+    @duration = duration
+    @@sessions[name] = self
+  end
+
+  # TODO: Use underscore stuff, make JS deal with RoR conventions?
+  def to_hash
+    {
+      name: name,
+      notificationTime: notification_time,
+      duration: duration
+    }
+  end
+
+  def self.find(name=nil)
+    name = "default" if !name || name.empty?
+    @@sessions[name] || self.new(name)
+  end
+
 end
