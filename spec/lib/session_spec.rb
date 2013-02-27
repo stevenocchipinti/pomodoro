48e4e9e78e7034ee0bd30f75740a6d25aa734a46
Steven Occhipinti <dev@stevenocchipinti.com>
2013-02-27 22:06:46 +1100

    Implement some unit tests for Session

--- spec/lib/session_spec.rb
+++ spec/lib/session_spec.rb
@@ -2,8 +2,24 @@ require './lib/session'
 
 describe Session do
   describe ".find" do
-    it "returns the requested session if it exists"
-    it "returns a new session if the requested session does not exist"
-    it "returns the default session if no other session is requested"
+
+    before do
+      Session.class_variable_set(:@@sessions, {})
+    end
+
+    it "returns the requested session if it exists" do
+      session = mock(Session)
+      Session.class_variable_set(:@@sessions, {"sesh" => session})
+      Session.find("sesh").should eq(session)
+    end
+
+    it "returns a new session if the requested session does not exist" do
+      Session.find("new-sesh").name.should eq("new-sesh")
+    end
+
+    it "returns the default session if no other session is requested" do
+      Session.find.name.should eq("default")
+      Session.find("").name.should eq("default")
+    end
   end
 end
