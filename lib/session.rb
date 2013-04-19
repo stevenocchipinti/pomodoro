07f1e1fceb12ce5c611b1efb529e6b5ddcfb92ce
Steven Occhipinti <dev@stevenocchipinti.com>
2013-04-20 00:42:00 +1000

    Adapt Session to suit the new plan
    The new plan includes:
    - Storing notification_time in the session, but returning time_left
    - Using Sinatra-Websocket instead of Pusher
      - This involves storing an array of sockets in the session

--- lib/session.rb
+++ lib/session.rb
@@ -1,31 +1,48 @@
 class Session
-  attr_accessor :name, :notification_time, :duration
+  attr_accessor :name, :duration, :notification_time, :connections
 
   @@sessions = {}
 
-  def initialize(name, notification_time=nil, duration=25)
+  def initialize(name, duration=25)
     @name = name
-    @notification_time = notification_time
     @duration = duration
+    @connections = []
     @@sessions[name] = self
   end
 
+  def start
+    @notification_time = Time.now + (@duration * 60)
+  end
+
+  def stop
+    @notification_time = nil
+  end
+
+
   def to_hash
     {
-      session: {
-        name: name,
-        notification_time: notification_time,
-        duration: duration
-      }
+      duration: duration,
+      time_left: time_left,
+      running: running?
     }
   end
+
+  def time_left
+    return nil unless notification_time
+    seconds_left = notification_time - Time.now
+    seconds_left > 0 ? seconds_left : 0
+  end
+
+  def running?
+    !!(notification_time && notification_time > Time.now)
+  end
+
 
   def self.all
     @@sessions
   end
 
-  def self.find(name=nil)
-    name = "default" if !name || name.empty?
+  def self.find_or_create(name)
     @@sessions[name] || self.new(name)
   end
 
--- spec/lib/session_spec.rb
+++ spec/lib/session_spec.rb
@@ -5,28 +5,104 @@ describe Session do
     Session.class_variable_set(:@@sessions, {})
   end
 
-  describe ".initialize" do
-    let(:sessions) { Session.class_variable_get(:@@sessions) }
+  describe "#initialize" do
+    it "sets the name and duration attributes to the given parameters" do
+      session = Session.new("my-session", 1)
+      session.name.should == "my-session"
+      session.duration.should == 1
+    end
+
+    it "sets the connections attribute to an empty array" do
+      session = Session.new("my-session", 1)
+      session.connections.should == []
+    end
 
     it "adds the new session object to a class-level hash of all sessions" do
-      expect{Session.find("new")}.to change{sessions.size}.by(1)
+      Session.class_variable_get(:@@sessions).should be_empty
+      Session.new("new")
+      Session.class_variable_get(:@@sessions).should have_key "new"
     end
   end
 
-  describe ".find" do
+  describe "#start" do
+    it "sets the notification_time to self.duration minutes from Time.now" do
+      Time.stub(:now).and_return(5)                 # seconds
+      session = Session.new("my-session", 1)        # minutes
+      session.start
+      session.notification_time.should == 65
+    end
+  end
+
+  describe "#stop" do
+    it "sets the notification_time to nil" do
+      Time.stub(:now).and_return(5)                 # seconds
+      session = Session.new("my-session", 1)        # minutes
+      session.start
+      session.notification_time.should_not be_nil
+      session.stop
+      session.notification_time.should be_nil
+    end
+  end
+
+
+  describe "#to_hash" do
+    subject { Session.new("my-session").to_hash }
+    it { should have_key :duration }
+    it { should have_key :time_left }
+    it { should have_key :running }
+  end
+
+  describe "#time_left" do
+    before do
+      Time.stub(:now).and_return(10)
+    end
+    let(:session) { Session.new("my-session") }
+
+    it "returns nil if the notification_time is not set" do
+      session.notification_time = nil
+      session.time_left.should be_nil
+    end
+
+    it "returns zero if the notification_time has passed" do
+      session.notification_time = 5
+      session.time_left.should eq(0)
+    end
+
+    it "returns the number of seconds remaining before the notification_time" do
+      session.notification_time = 25
+      session.time_left.should eq(15)
+    end
+
+  end
+
+  describe "#running?" do
+    let(:session) { Session.new("my-session") }
+
+    it "returns true when the notification time is in the future" do
+      session.notification_time = Time.now + 60*2   # 2 minutes
+      session.should be_running
+    end
+
+    it "returns false when the notification time is in the past" do
+      session.notification_time = Time.now - 60*2   # 2 minutes
+      session.should_not be_running
+    end
+    it "returns false when the notification time is nil" do
+      session.notification_time = nil
+      session.should_not be_running
+    end
+  end
+
+
+  describe ".find_or_create" do
     it "returns the requested session if it exists" do
       session = mock(Session)
       Session.class_variable_set(:@@sessions, {"sesh" => session})
-      Session.find("sesh").should eq(session)
+      Session.find_or_create("sesh").should eq(session)
     end
 
     it "returns a new session if the requested session does not exist" do
-      Session.find("new-sesh").name.should eq("new-sesh")
-    end
-
-    it "returns the default session if no other session is requested" do
-      Session.find.name.should eq("default")
-      Session.find("").name.should eq("default")
+      Session.find_or_create("new-sesh").should be_a Session
     end
   end
 
